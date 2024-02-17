55821967f861b44305158f2a57a96960d84cbec6
bartosz <bartosz@bartoszs-MacBook-Pro.local>
2024-02-17 02:48:42 +0100

    dev4passion_coffeecocoa_sellers

--- dev4passion_coffeecocoa_adminn/babel.config.js
+++
@@ -1,9 +0,0 @@
-// module.exports = {
-//   presets: ['module:@react-native/babel-preset'],
-// };
-
-
-module.exports = {
-  presets: ['module:@react-native/babel-preset'],
-  plugins: ['react-native-reanimated/plugin']
-};
---
+++ dev4passion_coffeecocoa_sellers/App.js
@@ -0,0 +1,150 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import type {Node} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+/* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
+ * LTI update could not be added via codemod */
+const Section = ({children, title}): Node => {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
+  );
+};
+
+
+const HomeScreen = () => {
+ return (
+     <Drawer.Navigator initialRouteName="Main">
+      <Drawer.Screen name="Main" component={MainScreen} />
+      <Drawer.Screen name="Account" component={Account} />
+     </Drawer.Navigator>
+ )
+}
+
+const SettingsScreen = () => {
+ return (
+  <View>
+   <Text>Settings Screen</Text> 
+</View>
+ )
+}
+
+const MainScreen = () => {
+ return (
+  <View><Text>Main Screen</Text></View>
+ )
+}
+
+const Account = () => {
+ return (
+  <View><Text>Account</Text></View>
+ )
+}
+
+
+const Feed = () => {
+ return (
+   <View>
+    <Text>Feed</Text>
+  </View>
+ )
+}
+
+ const Article = () => {
+  return (
+   <View>
+    <Text>Article</Text>
+   </View>
+  )
+ }
+
+
+const Drawer = createDrawerNavigator();
+const Tab = createBottomTabNavigator();
+
+const App: () => Node = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+
+    <NavigationContainer>
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Settings" component={SettingsScreen} />
+    </Tab.Navigator>
+    </NavigationContainer> 
+
+
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
